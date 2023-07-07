c89766e13860a0d6657460c9ec03b22ccff021f8
jang-yujin <jyj9747@naver.com>
2023-07-07 21:12:48 +0900

    :tada: Multi module 프로젝트 세팅
    참고 : https://www.youtube.com/watch?v=LEzQb0vSPbw

---
+++ module1/src/test/java/com/example/multimodule/module1/MultiModuleProjectFirstTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.multimodule.module1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiModuleProjectFirstTimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
